599bc510c694dbea236079c8dc199f7fc8711138
Vlad1m1rKolebakin <144666878+Vlad1m1rKolebakin@users.noreply.github.com>
2024-03-01 10:11:27 +0300

    Add files via upload

---
+++ CodewarsTasks/monkeyCount.js
@@ -0,0 +1,17 @@
+function monkeyCount(x){
+    let counter = 1;
+    const newArray = [];
+    for(let i=1; i<x+1;i++){
+        newArray.push(i);
+        counter++
+    };
+    return newArray;
+
+
+};
+
+
+
+
+
+
