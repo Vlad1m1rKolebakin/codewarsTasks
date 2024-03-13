07e2ac0d854066dc9fbbe387c38de0204a8eb2fd
Vlad1m1rKolebakin <144666878+Vlad1m1rKolebakin@users.noreply.github.com>
2024-03-13 10:01:42 +0300

    Add files via upload

---
+++ CodewarsTasks/Is this my tail?.js
@@ -0,0 +1,18 @@
+// function correctTail(body, tail){
+//     sub = body.substr(body.length-(tail.length))
+    
+//     if (sub === tail) {
+//         return true
+//     }
+//     else {
+//         return false
+
+//     }
+//   }
+    
+
+//   console.log(correctTail("Fox", "x"));
+
+
+
+const body = 'fox';
